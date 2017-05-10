7739a62ad13fb431e402a70669cb4accd9e2e2fc
Emmanuel <emmanuel.stroem@gmail.com>
2017-05-10 23:32:31 +0300

    added persistance data class object singleton and statically showing cover image

---
+++ PinterestValley/HomeViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewModel.swift
+//  PinterestValley
+//
+//  Created by Emmanuel on 08/05/2017.
+//  Copyright © 2017 Emmanuel. All rights reserved.
+//
+
+import Foundation
---
+++ PinterestValley/PinterestViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PinterestViewCell.swift
+//  PinterestValley
+//
+//  Created by Emmanuel on 10/05/2017.
+//  Copyright © 2017 Emmanuel. All rights reserved.
+//
+
+import UIKit
+
+class PinterestViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
